c869c22d61304170395ebf7cb2fda66c8b0c3b96
Marc BM <marcbajona1@gmail.com>
2019-11-25 12:06:46 +0100

    Lliurament 3 - 25 de Novembre (INACABAT)

--- app/src/main/java/com/bajonacastella/tecnogestio/MainActivity.java
+++ app/src/main/java/com/bajonacastella/tecnogestio/MainActivity.java
@@ -3,12 +3,36 @@ package com.bajonacastella.tecnogestio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_activity_actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_buscar:
+                Log.i("ActionBar", "Buscar!");;
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
