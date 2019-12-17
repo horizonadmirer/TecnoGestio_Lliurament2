33c16d5ccad3ce3bf43888f2b5dd125660c0ba29
Marc BM <marcbajona1@gmail.com>
2019-12-17 10:11:15 +0100

    Molts canvis diversos

--- app/src/main/java/com/bajonacastella/tecnogestio/MainActivity.java
+++ app/src/main/java/com/bajonacastella/tecnogestio/MainActivity.java
@@ -1,8 +1,11 @@
 package com.bajonacastella.tecnogestio;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -12,8 +15,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /**
@@ -25,17 +30,22 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private SQLiteDatabase db;
+    SharedPreferences prefs;
+    boolean benvinguda;
     String[] argsCategoria;
     ImageButton btnCat1,btnCat2,btnCat3,btnCat4,btnCat5,btnCat6;
     TextView txtDescripcioProducte1, txtNomProducte1;
+    ArrayAdapter adaptador;
+    ListView llista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         argsCategoria = new String[1];
-        txtNomProducte1 = findViewById(R.id.txtNomProducte1);
-        txtDescripcioProducte1 = findViewById(R.id.txtDescripcioProducte1);
+        //txtNomProducte1 = findViewById(R.id.txtPreu);
+        //txtDescripcioProducte1 = findViewById(R.id.txtPreu);
+        llista = findViewById(R.id.llistaBasica);
         btnCat1 = findViewById(R.id.categoria1);
         btnCat2 = findViewById(R.id.categoria2);
         btnCat3 = findViewById(R.id.categoria3);
@@ -50,6 +60,28 @@ public class MainActivity extends AppCompatActivity {
                 SQLiteDatabase.OPEN_READONLY
         );
 
+        prefs = getSharedPreferences("FitxerPreferencies",MODE_PRIVATE);
+        benvinguda = prefs.getBoolean("benvinguda",false);
+
+        if(!benvinguda)
+        {
+            new AlertDialog.Builder(this, R.style.AppTheme)
+                    .setTitle("Benvingut/da a TecnoGestió!")
+                    .setCancelable(false)
+                    .setMessage("Ara podràs consultar els productes per categoria bàsica o, si ho desitges, també " +
+                            "podràs fer cerques avançades. \n Gràcies per escollir la nostra aplicació!")
+                    .setPositiveButton("Molt bé, gràcies!", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            SharedPreferences.Editor editor = prefs.edit();
+                            editor.putBoolean("benvinguda", true);
+                            editor.apply();
+                        }
+                    })
+                    .show();
+        }
 
     }
 
@@ -83,58 +115,79 @@ public class MainActivity extends AppCompatActivity {
         if(v == btnCat1) {
             argsCategoria[0] = "1";
             Cursor c = db.query("productes", null, "categoria=?", argsCategoria, null, null, null);
-            c.moveToNext();
-            Log.e("BASE DE DADES", c.getString(1));
 
-            txtNomProducte1.setText(c.getString(1));
-            txtDescripcioProducte1.setText(c.getString(3));
+            llista.setAdapter(crearList(c));
         }
         if(v == btnCat2) {
             argsCategoria[0] = "2";
             Cursor c = db.query("productes", null, "categoria=?", argsCategoria, null, null, null);
-            c.moveToNext();
-            Log.e("BASE DE DADES", c.getString(1));
 
-            txtNomProducte1.setText(c.getString(1));
-            txtDescripcioProducte1.setText(c.getString(3));
+            llista.setAdapter(crearList(c));
         }
         if(v == btnCat3) {
             argsCategoria[0] = "3";
             Cursor c = db.query("productes", null, "categoria=?", argsCategoria, null, null, null);
-            c.moveToNext();
-            Log.e("BASE DE DADES", c.getString(1));
 
-            txtNomProducte1.setText(c.getString(1));
-            txtDescripcioProducte1.setText(c.getString(3));
+            llista.setAdapter(crearList(c));
         }
         if(v == btnCat4) {
             argsCategoria[0] = "4";
             Cursor c = db.query("productes", null, "categoria=?", argsCategoria, null, null, null);
-            c.moveToNext();
-            Log.e("BASE DE DADES", c.getString(1));
 
-            txtNomProducte1.setText(c.getString(1));
-            txtDescripcioProducte1.setText(c.getString(3));
+            llista.setAdapter(crearList(c));
         }
         if(v == btnCat5) {
             argsCategoria[0] = "5";
             Cursor c = db.query("productes", null, "categoria=?", argsCategoria, null, null, null);
-            c.moveToNext();
-            Log.e("BASE DE DADES", c.getString(1));
 
-            txtNomProducte1.setText(c.getString(1));
-            txtDescripcioProducte1.setText(c.getString(3));
+            llista.setAdapter(crearList(c));
         }
         if(v == btnCat6) {
             argsCategoria[0] = "6";
             Cursor c = db.query("productes", null, "categoria=?", argsCategoria, null, null, null);
-            c.moveToNext();
-            Log.e("BASE DE DADES", c.getString(1));
 
-            txtNomProducte1.setText(c.getString(1));
-            txtDescripcioProducte1.setText(c.getString(3));
+            llista.setAdapter(crearList(c));
         }
 
+    }
+
+    public ListViewPersonalitzada crearList (Cursor c) {
+        int numResultats = c.getCount();
+        ListViewPersonalitzada adaptador = null;
+        if(numResultats != 0) {
+            Log.e("REGISTRES: ", String.valueOf(c.getCount()));
+            //String[] values = new String[numResultats];
+            String[] nomProducte = new String[numResultats];
+            String[] descripcioProducte = new String[numResultats];
+            String[] marcaProducte = new String[numResultats];
+            String[] stockProducte = new String[numResultats];
+            String[] preuProducte = new String[numResultats];
+            Integer[] idImatge = new Integer[numResultats];
+            int i = 0;
+
+            //Movem el cursor a la primera posició.
+            if (c.moveToFirst()) {
+                do {
+                    /*// Construïm l'String amb tota l'informació corresponent al producte i fem un format més comprensible per l'usuari.
+                    values[i] = "\n" + c.getString(1) + "\n" + c.getString(3) + "\nPreu unitat: " + c.getString(5) + " €\n"
+                                + "Marca: "+  c.getString(2)  +"\t\tStock restant: " + c.getString(4) + "\n"; */
+
+                    nomProducte[i] = c.getString(1);
+                    descripcioProducte[i] = c.getString(3);
+                    marcaProducte[i] = c.getString(2);
+                    stockProducte[i] = c.getString(4);
+                    preuProducte[i] = c.getString(5) + " €";
+                    idImatge[i] = getResources().getIdentifier(c.getString(7), "drawable", getPackageName());
+                    //Log.e("IMATGE NOM", "" + idImatge[i]);
+
+                    i++;
+                } while (c.moveToNext());
+
+                adaptador = new ListViewPersonalitzada(this, nomProducte, descripcioProducte, marcaProducte, stockProducte, preuProducte, idImatge);
+            }
+        }
+        c.close();
+        return adaptador;
     }
 
 }
